227d64787d6253d34b353457598aeb5a5c66435d
Henri Bredt <development@skycodelabs.com>
2022-02-09 15:13:09 +0100

    index, work, sticky title

--- code/components/footer-component.js
+++ code/components/footer-component.js
@@ -1,7 +1,25 @@
 class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <p>I am a component footer</p>
+        <div class="container-max">
+        <hr>
+        <div class="container-full mb-1h mt-1h">
+           
+            <div class="container-third-left container-spacer-right">
+                <a href="#" class="a-secondary small-text nav-spacing">Henri Bredt</a>
+            </div>
+    
+            <div class="container-third-right container-spacer-left">
+                <div class="inline-container-left">
+                    <a href="#" class="a-secondary small-text nav-spacing">Legal</a>
+                    <a href="#" class="a-secondary small-text nav-spacing">Privacy</a>
+                    <a href="#" class="a-secondary small-text">Socials</a>
+                 </div>
+                
+            </div>
+    
+        </div>
+    </div>
         `
     }
 }
