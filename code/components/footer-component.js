5650c1be05ed963aa70374ddb3a5897b8dbf9af4
Henri Bredt <development@skycodelabs.com>
2022-02-12 00:54:57 +0100

    mvp index.html

--- code/components/footer-component.js
+++ code/components/footer-component.js
@@ -6,14 +6,14 @@ class Footer extends HTMLElement {
         <div class="container-full mb-1h mt-1h">
            
             <div class="container-third-left container-spacer-right">
-                <a href="#" class="a-secondary small-text nav-spacing">Henri Bredt</a>
+                <a href="#" class="a-primary small-text nav-spacing">Henri Bredt</a>
             </div>
     
             <div class="container-third-right container-spacer-left">
                 <div class="inline-container-left">
-                    <a href="#" class="a-secondary small-text nav-spacing">Legal</a>
-                    <a href="#" class="a-secondary small-text nav-spacing">Privacy</a>
-                    <a href="#" class="a-secondary small-text">Socials</a>
+                    <a href="#" class="a-primary small-text nav-spacing">Legal</a>
+                    <a href="#" class="a-primary small-text nav-spacing">Privacy</a>
+                    <a href="#" class="a-primary small-text">Socials</a>
                  </div>
                 
             </div>
