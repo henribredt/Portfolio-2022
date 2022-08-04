1e13857c890c9e8bf488c5da9f55ecc343452015
Henri <henribredt@web.de>
2022-08-04 11:55:11 +0200

    Improve about, hover buttons

--- code/components/nav-component.js
+++ code/components/nav-component.js
@@ -5,7 +5,7 @@ class Nav extends HTMLElement {
         <div class="container-max nav">
             <div class="container-full inline-container-right nav-container">
                 <a href="index.html" class="a-secondary small-text nav-spacing">Portfolio</a>
-                <a href="shop.html" class="a-primary small-text nav-spacing">Blog</a> 
+                <a href="https://henribredt.de/blog/index.html" target="_blank" class="a-primary small-text nav-spacing">Blog</a> 
                 <a href="shop.html" class="a-primary small-text">Shop</a> 
             </div>
         </div>
